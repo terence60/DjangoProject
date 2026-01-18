3d1e575d495b2810b4c5ea9e3b44ace1558ee856
Terence Dinguilu <terence.dinguilu@gmail.com>
2026-01-18 18:35:19 +0100

    views

--- employe/forms.py
+++ employe/forms.py
@@ -4,3 +4,24 @@ class EmployeForm(forms.ModelForm) :
     class Meta :
         model = Employe
         fields = ['nom' , 'email' , 'poste', 'salaire']
+        widgets = {
+            'nom' : forms.TextInput(attrs= {
+                'class' : 'input w-full' , 
+                'placeholder' : 'Nom'
+            }),
+
+              'email' : forms.TextInput(attrs= {
+                'class' : 'input w-full' , 
+                'placeholder' : 'Email'
+            }),
+              'poste' : forms.TextInput(attrs={
+                'class' : 'input w-full' , 
+                'placeholder' : 'Poste'
+            }),
+
+              'salaire' : forms.TextInput(attrs={
+                'class' : 'input w-full' , 
+                'placeholder' : 'Salaire'
+            })
+
+        }
--- employe/views.py
+++ employe/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render , redirect
+from django.shortcuts import render , redirect , get_object_or_404
 from .models import Employe
 from .forms import EmployeForm
 
@@ -14,4 +14,19 @@ def ajouter_employe (request):
         form.save()
         return redirect('liste_employes')
     return render(request , 'employe/formulaire.html' , {'form' : form})
+
+def modifier_employe (request, id) :
+    employe = get_object_or_404(Employe, id=id)
+    form = EmployeForm(request.POST or None, instance=employe)
+    if form.is_valid() :
+        form.save()
+        return redirect('liste_employes')
+    return render(request , 'employe/formulaire.html' , {'form' : form})
+
+def supprimer_employe (request, id) :
+    employe = get_object_or_404(Employe, id=id)
+    if request.method == "POST":
+        employe.delete()
+        return redirect('liste_employes')
+        
 
